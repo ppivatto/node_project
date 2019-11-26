872e4a46a2b455a0fda5601b617557a62e5d55dd
Pablo Pivatto <pepivatto@gmail.com>
2019-11-26 06:09:07 -0300

    var env

--- config/conexion.js
+++ config/conexion.js
@@ -1,6 +1,11 @@
+const argv = require('yargs').argv;
 let mongoose = require('mongoose');
 
-mongoose.connect('mongodb://pepito:test123@ds235239.mlab.com:35239/example-2018'); 
+const mongoConection = 'mongodb://pepito:test123@ds235239.mlab.com:35239/example-2018'
+
+console.log('MONGO CONNECTION>>', argv.environment)
+
+mongoose.connect(mongoConection); 
 //mongodb://localhost:27017/crud
 // 'mongodb://pepi:test123@port.mlab.com:15446/personas'
 
